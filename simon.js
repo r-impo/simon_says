06652de555ed67de804ffe4726fe5b9446d45cdd
Rodrigo Imposti <rimposti@gmail.com>
2016-09-05 00:49:47 -0300

    puliendo js
    todavia es un desastre esto. Seguir menos al parser y dejar que ejecute
    por porciones del script.

--- simon.js
+++ simon.js
@@ -19,13 +19,12 @@ var rojo = {
 	audio: new Audio("src/verde.wav")
 };
 
-var	score_elem = document.getElementById("score_track"),
-	contador = 0;
-var	arrayAI = [];
-var	arrayJug = [];
-var	arrayKeys = ["0","1","2","3"],
+var score_elem = document.getElementById("score_track"),
+	contador = 1,
+	arrayAI = [],
+	arrayJug = [],
 	turnSet = false;
-
+	
 function start(estado) { // empieza! Despliega puntaje 0 y pasa el turno a AI (true);
 	turnSet = estado;
 	oneMoreLoop(Math.floor(Math.random() * 4), contador);
@@ -34,87 +33,122 @@ function start(estado) { // empieza! Despliega puntaje 0 y pasa el turno a AI (t
 function reset() {
 	contador = 0;
 	turnSet = false;
+	arrayAI = [];
+	arrayJug = [];
 	score_elem.innerHTML = "Score: " + contador;
+	contador++
 }
 
-function listen4Player(cuenta) {
-	turnSet = true;
-	var timer = 10000 + (300 * cuenta);
-	setTimeout(function() {
-		if (arrayAI == arrayJug) {
-			score_elem.innerHTML = "Score: " + cuenta;
-			oneMoreLoop(Math.floor(Math.random() * 4), cuenta)
-		}
-		else {
-			console.log("PERDISTE!!");
-		}
-	}, timer);
-}
-
-function oneMoreLoop(random, cuenta) {
-	turnSet = false;
-	random.toString();
-	arrayAI.push(random);
-	console.log("Array AI: " + arrayAI);
+function timerForPlayer() {
 	for (i = 0; i < arrayAI.length; i++) {
-		setTimeout(function() {
-			console.log(arrayAI + " array AI");
-			playSound(arrayAI);
-		}, 600);
+		if (arrayJug[i] === arrayAI[i] && arrayJug.length == arrayAI.length) {
+			score_elem.innerHTML = "Score: " + contador;
+			setTimeout(oneMoreLoop, 2000);	
+		}
+		else if (arrayJug[i] != undefined && arrayJug[i] != arrayAI[i]) {
+			console.log("PERDISTE AMEO!!")
+			reset();
+		}
 	}
-	cuenta++
-	listen4Player(cuenta);
 }
 
-function playSound(nota) {
-	console.log("NOTA " + typeof nota);
-	switch(nota) {
-		case "0":
-			rojo.audio.play();
-			rojo.id.style.opacity = 0.8;
-			setTimeout(function() {
-				rojo.id.style.opacity = 1;
-			}, 600);
-			if (contador % 2 != 0) {
-				arrayJug.push(0);
-			}
-		break;
-
-		case "1":
-			azul.audio.play();
-			azul.id.style.opacity = 0.8;
-			setTimeout(function() {
-				azul.id.style.opacity = 1;
-			}, 600)
-			if (contador % 2 != 0) {
-				arrayJug.push(1);
-			}
-		break;
-
-		case "2":
-			amarillo.audio.play();
-			amarillo.id.style.opacity = 0.8;
-			setTimeout(function() {
-				amarillo.id.style.opacity = 1;
-			}, 600)
-			if (contador % 2 != 0) {
-				arrayJug.push(2);
-			}
-		break;
-
-		case "3":
-			verde.audio.play();
-			verde.id.style.opacity = 0.8;
-			setTimeout(function() {
-				verde.id.style.opacity = 1;
-			}, 600)
-			if (contador % 2 != 0) {
-				arrayJug.push(3);
-			}
-		break;
-
-		default:
-			console.log("ERROR!")
+function playerInput(value) {
+	if (turnSet == true) {
+		switch(value) {
+			case 0:
+				rojo.id.style.opacity = 0.8;
+				rojo.audio.play();
+				arrayJug.push(value);
+				setTimeout(function() {
+					rojo.id.style.opacity = 1;
+				}, 300);
 			break;
+			
+			case 1:
+				azul.id.style.opacity = 0.8;
+				azul.audio.play();
+				arrayJug.push(value);
+				setTimeout(function() {
+					azul.id.style.opacity = 1;
+				}, 300);
+			break;
+			
+			case 2:
+				amarillo.id.style.opacity = 0.8;
+				amarillo.audio.play();
+				arrayJug.push(value);
+				setTimeout(function() {
+					amarillo.id.style.opacity = 1;
+				}, 300);
+			break;
+			
+			case 3:
+				verde.id.style.opacity = 0.8;
+				verde.audio.play();
+				arrayJug.push(value);
+				setTimeout(function() {
+					verde.id.style.opacity = 1;
+				}, 300);			
+			break;
+			
+			default:
+				console.log("ERROR! Del lado del jugador.")	
+			break;	
+		}
 	}
-}	
+	else {
+		console.log("no es tu turno nabo");
+	}
+	timerForPlayer();
+}
+
+function oneMoreLoop() {
+	turnSet = false;
+	arrayAI.push(Math.floor(Math.random() * 4));
+	var notas = 0;
+	function playArray(notas) {
+		switch(arrayAI[notas]) {
+			case 0:
+				rojo.id.style.opacity = 0.8;
+				setTimeout(function() {
+					rojo.audio.play();
+					rojo.id.style.opacity = 1;
+				}, 300);
+				notas++
+			return setTimeout(playArray(notas), 1700);
+
+			case 1:
+				azul.id.style.opacity = 0.8;
+				setTimeout(function() {
+					azul.audio.play();
+					azul.id.style.opacity = 1;
+				}, 300);
+				notas++
+			return setTimeout(playArray(notas), 1700);
+
+			case 2:
+				amarillo.id.style.opacity = 0.8;
+				setTimeout(function() {
+					amarillo.audio.play();
+					amarillo.id.style.opacity = 1;
+				}, 300);
+				notas++
+			return setTimeout(playArray(notas), 1700);
+
+			case 3:
+				verde.id.style.opacity = 0.8;
+				setTimeout(function() {
+					verde.audio.play();
+					verde.id.style.opacity = 1;
+				}, 300);
+				notas++
+			return setTimeout(playArray(notas), 1700);
+
+			default:
+				turnSet = true;
+				return console.log("No hay mas array! Se cumplio el loop AI nro " + contador);
+			break;
+		}
+	}
+	playArray(notas)
+};
